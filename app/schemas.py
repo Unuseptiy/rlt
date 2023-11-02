00e51812b7688422229a276d095b22a33ca528e0
Kozenko Valeriy <Osmiylantan@mail.ru>
2023-11-02 16:41:03 +0300

    Lint project.

--- app/schemas.py
+++ app/schemas.py
@@ -20,6 +20,6 @@ class Request(BaseModel):
             "example": {
                 "dt_from": "2022-09-01T00:00:00",
                 "dt_upto": "2022-12-31T23:59:00",
-                "group_type": "month"
+                "group_type": "month",
             }
         }
